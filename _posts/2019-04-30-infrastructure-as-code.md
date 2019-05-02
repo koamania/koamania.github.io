7691f6d5d6a761ffe450eac3247c84fc2711ab51
dhlee <dhlee@miridih.com>
2019-05-02 09:16:45 +0900

    '포스트추가'

---
+++ _posts/2019-04-30-infrastructure-as-code.md
@@ -0,0 +1,23 @@
+---
+layout: default
+title: "코드로 인프라 관리하기"
+tags: DevOps
+---
+# Infra as code(IaC)란?
+
+-   인프라 자동화 방법
+-   시스템과 구성을 프로비저닝하고 일관되고 반복 가능한 절차를 코드로서 관리하는 방법
+
+## 목표
+
+-   인프라 변경이 장애물이나 제약이 되는 것이 아닌, 손 쉽게 변경을 지원하게 한다.
+-   시스템 변경은 스트레스나 고통을 주지 않는 작업이다.
+-   인프라 개선 작업은 대규모(Big bang) 프로젝트를 통해서가 아니라, 지속적이며 점진적인 절차를 통해서 개선한다.
+
+## 발생할 수 있는 문제점
+
+-   서버 폭증
+-   구성 편차
+-   눈송이 서버
+-   자동화 공포
+-   침식([링크](https://devcenter.heroku.com/articles/erosion-resistance#heroku-is-erosion-resistant))
--- assets/js/001.js
+++ assets/js/001.js
@@ -3,8 +3,13 @@
 
 var max = document.getElementsByClassName("btn")[1];
 var min = document.getElementsByClassName("btn")[2];
+var pageTitle = document.getElementsByClassName("post_title")[0];
+var isMaxized = false;
 
-function maximize () {
+function maximize (event) {
+	if (event) {
+		event.stopPropagation();
+	}
 	var post = document.getElementsByClassName("content")[0];
 	var cont = document.getElementsByClassName("post_content")[0];
 	var wid = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth;
@@ -18,10 +23,15 @@ function maximize () {
 		} else {
 			cont.style.width = "99.4%";
 		}
+
+		isMaxized = true;
 	}
 }
 
-function minimize () {
+function minimize (event) {
+	if (event) {
+		event.stopPropagation();
+	}
 	var post = document.getElementsByClassName("content")[0];
 	var cont = document.getElementsByClassName("post_content")[0];
 	var wid = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth;
@@ -29,8 +39,17 @@ function minimize () {
 	if ( wid > 900 ) {
 		post.style.width = "800px";
 		cont.style.width = "98.5%";
+
+		isMaxized = false;
 	}
 }
 
 max.addEventListener('click', maximize, false);
 min.addEventListener('click', minimize, false);
+pageTitle.addEventListener('dblclick', function(e) {
+	if (isMaxized) {
+		minimize(e);
+	} else {
+		maximize(e);
+	}
+}, false);
